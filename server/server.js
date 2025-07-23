f91e3e9a262c95c1101b93482bc9f4652e6cc01b
Ademola-giod <idowuademola65@gmail.com>
2025-07-23 11:34:26 +0100

    added render.yaml for hosting on render and made changes to server.js

--- server/server.js
+++ server/server.js
@@ -85,4 +85,4 @@ app.use("/api", paymentRoutes);
 
 const PORT = process.env.PORT || 5000;
 // app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-export default app; // for vercel to recognize app
+// export default app; // for vercel to recognize app
