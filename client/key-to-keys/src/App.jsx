74b50dcd30b040034855fbf7e5e5c3a8433478f5
Ademola-giod <idowuademola65@gmail.com>
2025-07-24 15:30:55 +0100

    npm install @vercel/speed-insights --legacy-peer-deps

--- client/key-to-keys/src/App.jsx
+++ client/key-to-keys/src/App.jsx
@@ -3,7 +3,6 @@ import { ToastContainer } from "react-toastify"; //toastify
 import "react-toastify/dist/ReactToastify.css"; //
 import Home from './components/Home.jsx'
 import CoursePage from "./components/pages/getcourses/CoursePage.jsx";
-// import VerifyRedirect from "./components/pages/getcourses/VerifyRedirect.jsx";
 import ThankYou from "./components/pages/getcourses/ThankYou.jsx";
 
 
@@ -13,7 +12,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} /> 
         <Route path="/courses" element={<CoursePage />} />
-        {/* <Route path="/verify-redirect" element={<VerifyRedirect/>} /> */}
         <Route path="/thank-you" element={<ThankYou/>} />
           
 
