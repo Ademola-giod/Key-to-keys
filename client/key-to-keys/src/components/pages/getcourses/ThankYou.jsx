13f05c6980c88915da96d3508ccb6fb8eb9e1009
Ademola-giod <idowuademola65@gmail.com>
2025-07-25 21:30:20 +0100

    just updated the warning text

--- client/key-to-keys/src/components/pages/getcourses/ThankYou.jsx
+++ client/key-to-keys/src/components/pages/getcourses/ThankYou.jsx
@@ -93,7 +93,7 @@ const ThankYou = () => {
           </a>
         </div>
 
-        <div className=" mt-6 text-center text-sm text-primary max-w-md px-4">
+        <div className=" mt-6 text-center text-sm max-w-md px-4">
         <p className="italic">
           ⚠️ Full access will be granted in few minutes. You can also return to the homepage to access your course.
         </p>
