3ec648637cb990a492d3d6f9a0b575fc246addac
ademola <idowuademola65@gmail.com>
2026-09-19 09:37:48 +0100

    fix: edited footer keystokeys

--- client/key-to-keys/src/components/pages/Home/Footer.jsx
+++ client/key-to-keys/src/components/pages/Home/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <footer className="bg-primary text-white py-6 px-5">
       <div className="max-w-6xl mx-auto flex flex-col md:flex-row justify-between items-center text-sm">
         <div className="mb-2 md:mb-0 font-semibold">🔑 KEY TO KEYS</div>
-        <div>Copyright © {new Date().getFullYear()} Keys to Keys, all rights reserved</div>
+        <div>Copyright © {new Date().getFullYear()} Key to Keys, all rights reserved</div>
         <div className="flex gap-4 mt-2 md:mt-0">
           <FaInstagram className="hover:text-black" />
           <FaEnvelope className="hover:text-black" />
